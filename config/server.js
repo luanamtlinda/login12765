1d24051456eb8c1677d5d591ab78acda09cb05e9
luanamtlinda <61693923+luanamtlinda@users.noreply.github.com>
2021-09-30 08:56:17 -0300

    Add files via upload

---
+++ config/server.js
@@ -0,0 +1,6 @@
+var express = require('express')
+var app = express()
+
+var porta = process.env.PORT || 3030
+
+module.exports = {app,porta}
---
+++ index.js
@@ -0,0 +1,10 @@
+var server = require('./config/server')
+var app = server.app
+var porta = server.porta
+
+
+var consign = require('consign')
+consign().include('./routes').into(app)
+
+
+app.listen(porta)
