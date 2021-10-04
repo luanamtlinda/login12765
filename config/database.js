91a633d9f26064f5a3702a4dc02fc080df177c61
luanamtlinda <61693923+luanamtlinda@users.noreply.github.com>
2021-10-04 12:56:46 -0300

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 
 var conexao = ()=>{
-    mongoose.connect('mongodb+srv://<username>:<password>@cluster0.wqg7u.mongodb.net/login_registro?retryWrites=true&w=majority')
+    mongoose.connect('mongodb+srv://registrouser:luaninha22@cluster0.wqg7u.mongodb.net/login_registro?retryWrites=true&w=majority')
 }
 module.exports = conexao
--- config/server.js
+++ config/server.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
 
+app.use(express.urlencoded({extended:false}))
+
+
 var porta = process.env.PORT || 3030
 
 module.exports = {app,porta}
